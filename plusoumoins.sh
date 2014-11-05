8e81a2be98a8c23eeae8e708a7bf66c6bafa8968
flaman_m <flaman_m@f477bc3e-e266-465e-bd39-c37d095c9186>
2014-11-05 15:17:20 +0000

    programme

--- plusoumoins.sh
+++ plusoumoins.sh
@@ -5,33 +5,89 @@
 ## Login   <flaman_m@etna-alternance.net>
 ## 
 ## Started on  Mon Nov  3 16:33:12 2014 FLAMANT Morgane
-## Last update Tue Nov  4 11:43:45 2014 FLAMANT Morgane
+## Last update Wed Nov  5 16:20:36 2014 FLAMANT Morgane
 ##
 
 nbr=0
+mine=0
+
 nombre=$RANDOM
 let "nombre %= 100"
+nh=0
+nbj=1
+echo "Nombre de joueur ? 1/2"
+read nbj
+
+while [ $nbj -gt 2 ] || [ $nbj -lt 1 ]
+do echo "Min 1 joueur , max 2 joueurs"
+read nbj
+done
+clear
+
+if [ $nbj -eq 2 ]
+then
+echo "Joueur 1 entre un nombre entre 0 et 100"
+stty -echo
+read nombre
+stty echo 
+echo "On joue avec des mines? o/n"
+read mine
+while [ $nombre -gt 100 ] || [ $nombre -lt 0]
+do
+echo "Entre 0 et 100"
+stty -echo
+read nombre
+stty echo
+done
+clear
+echo "Joueur 2 joue"
+nh=1
+else 
+clear
 echo "Bonjour, $USER,"
-while test $nbr != 10
-do echo "Choisissez un nombre entre 0 et 100"
-   let "got = 10 - $nbr"
-    if [ $got -eq '1' ]
+echo "on joue avec des mines o/n"
+read mine
+
+echo "A vous de jouer"
+fi
+
+while test $nbr != 10  
+do echo "Aller on tape un nombre"
+    if [ "$mine" = "o" ]
     then
-	echo "Nombres d'essais restant = $got"
-    else
-        echo "Nombres d'essais restant= $got"
+    nbr2=$RANDOM
+    let "nbr2 %= 100"
+    else 
+    nbr2=1000
+    fi
+   
+    let "got = 10 - $nbr"
+    if [  $got -gt 1 ]
+    then
+    echo "Essais restant = $got"
+    else 
+	echo "Essai restant = $got"
     fi
     read chiffre
-    if [ "$chiffre" = "exit" ]
+    if [ "$chiffre" = "$nbr2"  ]
     then
-        exit
+	echo "Oooops, vous etes tomber sur une mine"
+	exit
     fi
-    if [ $chiffre ]
+    
+    if [ "$chiffre" = "exit" ] 
     then
-    tmp=$(($chiffre * 1))
+	exit
+    fi
+    
+    regex="^([0-9])"
+    
+    if [[ "$chiffre" =~ $regex ]]
+    then
+    tmp=$(($chiffre * 1))    
     if [ "$chiffre" != "$tmp" ]
     then
-        echo "Vous arrivez a calculer a+a ?"
+        echo "On a dit une valeur entre 0 et 100"
         nbr=$(($nbr-1))
     elif [ $chiffre -lt 0 ] || [ $chiffre -gt 100 ]
     then
@@ -39,21 +95,43 @@ do echo "Choisissez un nombre entre 0 et 100"
         nbr=$(($nbr-1))
     elif [ $chiffre -ge 0 ] && [ $chiffre -le 100 ]
     then
-        if [ $chiffre -eq $nombre ]
-            then
-            echo "Felicitation ! La solution est la ! "
-            exit
-        elif [ $chiffre -lt $nombre ]
-        then
-            echo "C'est plus grand"
-        else
-            echo "C'est plus petit"
-        fi
-      fi
-else
-    echo "J'attends une valeur"
-nbr=$(($nbr-1))
+	if [ $chiffre -eq $nombre ]
+	then
+	    clear
+	    nbr=$(($nbr+1))
+	    echo "Felicitation c'est le nombre"
+	    echo "$nbr coup jouer"
+	    nbf=1
+	    if [ $nh -eq 0 ] ; then
+		if [ 'find ./test = 0'  ] ; then
+		    echo "$USER = $nbr coup jouer" >> test
+		else
+		    exec 3<test
+		    while read line 0<&3
+		    do
+			nbf=$(($nbf+1))
+			
+		    done
+		    echo "$USER = $nbr coup jouer" >> test
+		    echo "score:"
+		    
+		fi
+		cat ./test  
+	    fi
+	    exit
+	elif [ $chiffre -lt $nombre ]
+	then
+	    echo "C'est plus grand"
+	else
+	    echo "C'est plus petit"
 fi
-    nbr=$(($nbr+1))
+    fi
+    else
+	echo "J'attend une valeurs"
+	nbr=$(($nbr-1))
+    fi
+    nbr=$(($nbr+1)) 
+    
 done
+clear
 echo "You loose"
